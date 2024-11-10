346ecfd6d49aee15bf3423f4dc483e6d74a4644b
Deepanksha <deepanksha1992@gmail.com>
2024-11-10 09:18:42 +0530

    dev h1 and div added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <>
+      <h1>hello</h1>
       <h2>Header of website</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
         nihil!
       </p>
+      <div>changes at the end</div>
     </>
   );
 };
