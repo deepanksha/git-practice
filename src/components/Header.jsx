75a74792903909795221f6c322b7fde1d9d5e88d
Deepanksha <deepanksha1992@gmail.com>
2024-11-10 09:24:23 +0530

    title added at master branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <>
+      <h1>heading added</h1>
       <h2>Header of website</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
         nihil!
       </p>
+      <div>another paragraph added</div>
     </>
   );
 };
